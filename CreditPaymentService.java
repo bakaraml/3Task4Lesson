12fec5c5df6cc88867b9b3bd30517bcc5cf4ce65
bakaraml <70254247+bakaraml@users.noreply.github.com>
2020-09-19 19:41:54 +0300

    Update CreditPaymentService.java

--- CreditPaymentService.java
+++ CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
     public double calculate (long amount, double percent, long mounth) {
 
-        double percent1 = persent / 100 / 12;
+        double percent1 = percent / 100 / 12;
 
-        double total = amount * (persent1 + (persent1 / ((Math.pow((1 + persrnt1), mounth)) - 1)));
+        double total = amount * (percent1 + (percent1 / ((Math.pow((1 + percent1), mounth)) - 1)));
 
         return total;
     }
